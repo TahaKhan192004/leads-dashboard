3772a6fdd147def0e33c2d9e8c29431187a0a1af
TahaKhan192004 <tahakhan172004@gmail.com>
2026-03-20 23:03:20 +0500

    fixed the mixed content error

--- leads-dashboard/next.config.js
+++ leads-dashboard/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/backend/:path*",
+        destination: "http://3.137.156.82:8000/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
